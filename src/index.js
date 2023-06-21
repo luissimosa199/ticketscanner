84aacc5534b96c0f8bcf602a0615046bc053dcd1
Luis Simosa <simosa37@gmail.com>
2023-06-21 09:52:29 -0300

    fix: imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+import express from "express";
+import { app } from "express";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
 
--- src/v1/parsers/ticketParser.js
+++ src/v1/parsers/ticketParser.js
@@ -1,5 +1,5 @@
-const { JSDOM } = require("jsdom");
-const fetch = require("node-fetch");
+import { JSDOM } from "jsdom";
+import fetch from "node-fetch";
 
 // SCAN ONE BILL
 const scanNewDiscoTicket = async (ticketUrl) => {
