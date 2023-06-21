b99c51f08f531741b0b4e0b6c88a0efcf2ca71fa
Luis Simosa <simosa37@gmail.com>
2023-06-21 09:58:30 -0300

    fix: cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const v1TicketRouter = require("./v1/routes/ticketRoutes");
 
 // cors
-app.use(cors());
+app.use(cors('*'));
 // headers
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
