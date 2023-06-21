21a65b5d33eab59df7a98341619cdb2e066b7c86
Luis Simosa <simosa37@gmail.com>
2023-06-20 23:39:00 -0300

    headers added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const v1TicketRouter = require("./v1/routes/ticketRoutes");
 
 // cors
 app.use(cors());
+// headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 // parse json
