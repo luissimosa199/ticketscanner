7a559f5c4638cc328dea7498d37499121542ee80
Luis Simosa <simosa37@gmail.com>
2023-06-21 09:33:56 -0300

    fix: cors error

--- src/index.js
+++ src/index.js
@@ -7,18 +7,22 @@ const PORT = process.env.PORT || 5000;
 const v1TicketRouter = require("./v1/routes/ticketRoutes");
 
 // cors
-app.use(cors());
+app.use(cors('*'));
 // headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
-app.options('/api/v1/tickets/disco', (req, res) => {
-  res.sendStatus(200);
-});
+// app.options('/api/v1/tickets/disco', (req, res) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.sendStatus(200);
+// });
+
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 // parse json
